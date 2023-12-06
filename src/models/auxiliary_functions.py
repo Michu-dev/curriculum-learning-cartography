0dfa5a9363301988551d6e9fea46ee4c36f91979
Michał Ciesielski <michalciesielski2000@gmail.com>
2023-12-06 21:35:28 +0100

    feat: build the general neural network model

---
+++ src/models/auxiliary_functions.py
@@ -0,0 +1,35 @@
+import torch
+
+
+def get_default_device():
+    """Pick GPU if available, else CPU"""
+    if torch.cuda.is_available():
+        return torch.device('cuda')
+    else:
+        return torch.device('cpu')
+
+def to_device(data, device):
+    """Move tensor(s) to chosen device"""
+    if isinstance(data, (list,tuple)):
+        return [to_device(x, device) for x in data]
+    return data.to(device, non_blocking=True)
+
+def get_optimizer(model, lr = 0.001, wd = 0.0):
+    parameters = filter(lambda p: p.requires_grad, model.parameters())
+    optim = torch.optim.Adam(parameters, lr=lr, weight_decay=wd)
+    return optim
+
+class DeviceDataLoader():
+    """Wrap a dataloader to move data to a device"""
+    def __init__(self, dl, device):
+        self.dl = dl
+        self.device = device
+        
+    def __iter__(self):
+        """Yield a batch of data after moving it to device"""
+        for b in self.dl: 
+            yield to_device(b, self.device)
+
+    def __len__(self):
+        """Number of batches"""
+        return len(self.dl)
---
+++ src/models/generalised_neural_network_model.py
@@ -0,0 +1,33 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class GeneralisedNeuralNetworkModel(nn.Module):
+    def __init__(self, embedding_sizes, n_cont):
+        super().__init__()
+        self.embeddings = nn.ModuleList([nn.Embedding(categories, size) for categories, size in embedding_sizes])
+        n_emb = sum(e.embedding_dim for e in self.embeddings)
+        self.n_emb, self.n_cont = n_emb, n_cont
+        self.lin1 = nn.Linear(self.n_emb + self.n_cont, 200)
+        self.lin2 = nn.Linear(200, 70)
+        self.lin3 = nn.Linear(70, 1)
+        self.bn1 = nn.BatchNorm1d(self.n_cont)
+        self.bn2 = nn.BatchNorm1d(200)
+        self.bn3 = nn.BatchNorm1d(70)
+        self.emb_drop = nn.Dropout(0.6)
+        self.drops = nn.Dropout(0.3)
+
+    def forward(self, x_cat, x_cont):
+        x = [e(x_cat[:, i]) for i, e in enumerate(self.embeddings)]
+        x = torch.cat(x, 1)
+        x = self.emb_drop(x)
+        x2 = self.bn1(x_cont)
+        x = torch.cat([x, x2], 1)
+        x = F.relu(self.lin1(x))
+        x = self.drops(x)
+        x = self.bn2(x)
+        x = F.relu(self.lin2(x))
+        x = self.drops(x)
+        x = self.bn3(x)
+        x = F.sigmoid(self.lin3(x))
+        return x
