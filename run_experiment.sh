0a1888a88ad36df4c241bdbd880df3b45413d073
Michał Ciesielski <michalciesielski2000@gmail.com>
2024-05-24 12:24:46 +0200

    feat: add plotting of learning curves and t-test features for experiments results

--- run_experiment.sh
+++ run_experiment.sh
@@ -8,12 +8,12 @@ do
     echo "ITERATION: $n"
     for d in airline_passenger_satisfaction stellar credit_card fashion_mnist
     do
-        # echo "Setup: dataset - $d, ranking_mode: None, relaxed - False, ranked - False"
-        # python -m src.train_model -d $d -e 8 -g 1.5 
+        echo "Setup: dataset - $d, ranking_mode: None, relaxed - False, ranked - False"
+        python -m src.train_model -d $d -e 8 -g 1.5 
         for m in cartography confidence
         do
-            # echo "Setup: dataset - $d, ranking_mode: $m, relaxed - False, ranked - True"
-            # python -m src.train_model -d $d -m $m -e 8 -g 1.5 -a 0.1 -bt 2.5 -rnk
+            echo "Setup: dataset - $d, ranking_mode: $m, relaxed - False, ranked - True"
+            python -m src.train_model -d $d -m $m -e 8 -g 1.5 -a 0.1 -bt 2.5 -rnk
             for g in 1.5 2 2.5 5
             do
                 echo "Setup: dataset - $d, ranking_mode: $m, relaxed - True, ranked - False, gamma - $g"
